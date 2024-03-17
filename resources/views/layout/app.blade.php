754635b8f039b738215bc2bad444664a4695f4cc
lucapolzone <lucapolzone@gmail.com>
2024-03-18 00:16:17 +0100

    feat: add jumbotron

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  {{-- Google font --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
   <title>Titolo - @yield('title')</title>
 
   @vite('resources/js/app.js')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,47 +8,51 @@
     <nav class="d-flex justify-content-center align-items-center">
       <img src="{{ Vite::asset('/resources/images/dc-logo.png') }}" class="dc-logo" alt="">
 
-      <ul>
+      <ul class="d-flex">
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'home']) href="{{ route('home') }}">Homepage</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'home']) href="{{ route('home') }}">HOME</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'about']) href="{{ route('about') }}">About</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'about']) href="{{ route('about') }}">ABOUT</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'contacts']) href="{{ route('contacts') }}">Contacts</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'contacts']) href="{{ route('contacts') }}">CONTACTS</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'comics']) href="{{ route('comics') }}">Comics</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'comics']) href="{{ route('comics') }}">COMICS</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'movies']) href="{{ route('movies') }}">Movies</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'movies']) href="{{ route('movies') }}">MOVIES</a>
         </li>
         <li>
           <a @class (['link','active' => Route::currentRouteName() == 'tv']) href="{{ route('tv') }}">TV</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'games']) href="{{ route('games') }}">Games</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'games']) href="{{ route('games') }}">GAMES</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'collectibles']) href="{{ route('collectibles') }}">Collectibles</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'collectibles']) href="{{ route('collectibles') }}">COLLECTIBLES</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'videos']) href="{{ route('videos') }}">Videos</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'videos']) href="{{ route('videos') }}">VIDEOS</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'fans']) href="{{ route('fans') }}">Fans</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'fans']) href="{{ route('fans') }}">FANS</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'news']) href="{{ route('news') }}">News</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'news']) href="{{ route('news') }}">NEWS</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'shop']) href="{{ route('shop') }}">Shop</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'shop']) href="{{ route('shop') }}">SHOP</a>
         </li>
         <li>
-          <a @class (['link','active' => Route::currentRouteName() == 'characters']) href="{{ route('characters') }}">Characters</a>
+          <a @class (['link','active' => Route::currentRouteName() == 'characters']) href="{{ route('characters') }}">CHARACTERS</a>
         </li>
       </ul>
     </nav>
+  </div>
+
+  <div class="jumbotron">
+    <img src="{{ Vite::asset('/resources/images/hero-image.jpg') }}" class="hero-image" alt="">
   </div>
 </header>
