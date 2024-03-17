c4801866e5f83696aed65a8c88115c0a2ab0dc44
lucapolzone <lucapolzone@gmail.com>
2024-03-17 16:06:25 +0100

    feat: add Route::currentRouteName()

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Titolo della pagina - @yield('title')</title>
+  <title>Titolo - @yield('title')</title>
 
   @vite('resources/js/app.js')
   @yield('css')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,18 @@
-<header>
+<!-- Route::currentRouteName() restituisce il nome della rotta corrente -->
+
+<header class="container d-flex justify-content-center">
   <ul>
     <li>
-      <a href="{{ route('home') }}">Homepage</a>
-      <a href="{{ route('about') }}">About</a>
-      <a href="{{ route('contacts') }}">Contacts</a>
-      <a href="{{ route('products') }}">Products</a>
+      <a @if (Route::currentRouteName() == 'home') class="link active" @endif href="{{ route('home') }}">Homepage</a>
+    </li>
+    <li>
+      <a @if (Route::currentRouteName() == 'about') class="link active" @endif href="{{ route('about') }}">About</a>
+    </li>
+    <li>
+      <a @if (Route::currentRouteName() == 'contacts') class="link active" @endif href="{{ route('contacts') }}">Contacts</a>
+    </li>
+    <li>
+      <a @if (Route::currentRouteName() == 'products') class="link active" @endif href="{{ route('products') }}">Products</a>
     </li>
   </ul>
 </header>
