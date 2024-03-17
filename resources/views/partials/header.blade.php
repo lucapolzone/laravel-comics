49fc6743eb9e0b361b30c3eb8c85d691174db9ae
lucapolzone <lucapolzone@gmail.com>
2024-03-17 16:19:44 +0100

    feat: add @class

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,16 +3,16 @@
 <header class="container d-flex justify-content-center">
   <ul>
     <li>
-      <a @if (Route::currentRouteName() == 'home') class="link active" @endif href="{{ route('home') }}">Homepage</a>
+      <a @class (['link','active' => Route::currentRouteName() == 'home']) href="{{ route('home') }}">Homepage</a>
     </li>
     <li>
-      <a @if (Route::currentRouteName() == 'about') class="link active" @endif href="{{ route('about') }}">About</a>
+      <a @class (['link','active' => Route::currentRouteName() == 'about']) href="{{ route('about') }}">About</a>
     </li>
     <li>
-      <a @if (Route::currentRouteName() == 'contacts') class="link active" @endif href="{{ route('contacts') }}">Contacts</a>
+      <a @class (['link','active' => Route::currentRouteName() == 'contacts']) href="{{ route('contacts') }}">Contacts</a>
     </li>
     <li>
-      <a @if (Route::currentRouteName() == 'products') class="link active" @endif href="{{ route('products') }}">Products</a>
+      <a @class (['link','active' => Route::currentRouteName() == 'products']) href="{{ route('products') }}">Products</a>
     </li>
   </ul>
 </header>
